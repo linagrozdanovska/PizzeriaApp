2c830118c7307e3bbc839cb59972d809ba33bed8
Гроздановска Лина <lina.grozdanovska@students.finki.ukim.mk>
2021-09-27 03:47:09 +0200

    Added Account controller, registration & login

---
+++ PizzeriaApp.Web/Models/Identity/UserRegistrationDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketsPurchase.Domain.Identity
+{
+    public class UserRegistrationDto
+    {
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
